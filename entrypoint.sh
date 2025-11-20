b571218ce4470f08d50d62dce4425cfcb7a094c8
Jessica Severin <jessica.severin@riken.jp>
2025-11-20 16:50:13 +0900

    fixed the mariadb init so it works in the build process and we don't need to hack it here in the entrypoint. also added an interactive shell at the end to help with development. I might need to remove the shell for production

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#chown -R mysql:mysql /var/lib/mysql /var/run/mysqld 
 service mariadb restart
-/sbin/init_db.sh
 service apache2 restart
 #zenbu_agent_launcher.sh
 
+echo "Entrypoint ended";
+/bin/bash "$@"
